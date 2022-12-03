122d72d19ffb98194d7a206434af23c2d346b84c
Michał Jurewicz <malauch@emtb.pl>
2022-12-03 11:00:54 +0100

    Removed TCA product dependency.

--- Package.swift
+++ Package.swift
@@ -3,35 +3,35 @@
 import PackageDescription
 
 let package = Package(
-  name: "composable-core-location",
-  platforms: [
-    .iOS(.v13),
-    .macOS(.v10_15),
-    .tvOS(.v13),
-    .watchOS(.v6),
-  ],
-  products: [
-    .library(
-      name: "ComposableCoreLocation",
-      targets: ["ComposableCoreLocation"]
-    )
-  ],
-  dependencies: [
-    .package(
-      url: "https://github.com/pointfreeco/swift-composable-architecture",
-      .upToNextMajor(from: "0.43.0"))
-  ],
-  targets: [
-    .target(
-      name: "ComposableCoreLocation",
-      dependencies: [
-        .product(name: "Dependencies", package: "swift-composable-architecture"),
-        .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
-      ]
-    ),
-    .testTarget(
-      name: "ComposableCoreLocationTests",
-      dependencies: ["ComposableCoreLocation"]
-    ),
-  ]
+	name: "composable-core-location",
+	platforms: [
+		.iOS(.v13),
+		.macOS(.v10_15),
+		.tvOS(.v13),
+		.watchOS(.v6),
+	],
+	products: [
+		.library(
+			name: "ComposableCoreLocation",
+			targets: ["ComposableCoreLocation"]
+		)
+	],
+	dependencies: [
+		.package(
+			url: "https://github.com/pointfreeco/swift-composable-architecture",
+			.upToNextMajor(from: "0.43.0"))
+	],
+	targets: [
+		.target(
+			name: "ComposableCoreLocation",
+			dependencies: [
+				.product(name: "Dependencies", package: "swift-composable-architecture"),
+//        .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
+			]
+		),
+		.testTarget(
+			name: "ComposableCoreLocationTests",
+			dependencies: ["ComposableCoreLocation"]
+		),
+	]
 )

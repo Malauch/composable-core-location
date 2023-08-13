a94723ac9278db5f1a3a68ee299a01187c861333
Michał Jurewicz <malauch@emtb.pl>
2023-08-13 13:32:48 +0200

    Restored failing (unimplemented) instance for test testValue

--- Sources/ComposableCoreLocation/Dependencies.swift
+++ Sources/ComposableCoreLocation/Dependencies.swift
@@ -11,5 +11,6 @@ extension DependencyValues {
 extension LocationManager: DependencyKey {
 	public static var liveValue: Self { Self.live() }
 	public static var previewValue: Self { Self.noop }
+	public static var testValue: Self { Self.unimplemented }
 	
 }
